e63d5b9b73f82a28b00356d8aac0067e2edd5d79
Daniel Sanchez <programazion1@outlook.com>
2024-07-22 06:14:56 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//queryselector para tomar un selector 
+let titulo = document.querySelector('h1');
+titulo.innerHTML='Juego del número Secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML='Indica un número del 1 al 10';
+//función para los botones 
+function intentoDeUsuario(){
+    alert('click desde el botón');
+}
